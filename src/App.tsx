1bc9bbee21651c7c1aa8dfa56a720e0c26ee7225
zouyujun <zouyujun_816@163.com>
2024-02-18 21:20:00 +0800

    feat：添加gui调试面板

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader';
+import { GUI } from 'dat.gui';
 
 function App() {
   const [width, setWidth] = useState(window.innerWidth);
@@ -16,9 +17,11 @@ function App() {
   let camera = useRef<THREE.Camera>();
   let renderer = useRef<THREE.Renderer>();
   let controls = useRef<OrbitControls>();
-
-  let donuts;
-  let mixer;
+  let sch;
+  // 灯光控制
+  const lighter = {};
+  // 调试ui
+  let gui = null;
 
   const resizeUpdate = (e: any) => {
     // 通过事件对象获取浏览器窗口的高度
@@ -31,8 +34,8 @@ function App() {
     setWidth(w);
   };
 
-  // 初始化---重置窗口大小
-  useEffect(() => {
+  // 初始化
+  const init = () => {
     // 页面刚加载完成后获取浏览器窗口的大小
     let h = window.innerHeight;
     setHeight(h);
@@ -40,14 +43,7 @@ function App() {
     setWidth(w);
     // 页面变化时获取浏览器窗口的大小
     window.addEventListener('resize', resizeUpdate);
-    return () => {
-      // 组件销毁时移除监听事件
-      window.removeEventListener('resize', resizeUpdate);
-    };
-  }, []);
 
-  // 初始化/销毁 应用
-  useEffect(() => {
     // 创建场景
     scene.current = new THREE.Scene();
     // 创建相机
@@ -57,61 +53,104 @@ function App() {
       0.1,
       1000,
     );
+    // 创建渲染器
     renderer.current = new THREE.WebGLRenderer({ antialias: true }); // 打开抗锯齿
     renderer.current.setSize(width, height);
 
     document.getElementById('container').appendChild(renderer.current.domElement);
     // 设置相机位置
-    camera.current.position.set(1, 1, 1);
-
+    camera.current.position.set(1, 1, 250);
     controls.current = new OrbitControls(camera.current, renderer.current.domElement);
 
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
-    scene.current.add(ambientLight);
-    // const directLight = new THREE.DirectionalLight(0xffffff, 2);
-    // directLight.position.set(10, 0, 200);
-    // scene.current.add(directLight);
     // 添加坐标系
-    // const axesHelper = new THREE.AxesHelper(5);
-    // scene.current.add(axesHelper);
+    const axesHelper = new THREE.AxesHelper(500);
+    scene.current.add(axesHelper);
 
+    initLight();
+    initGUI();
+  };
+
+  useEffect(() => {
+    init();
     // 场景添加glb文件
     gltfLoader();
     // 添加背景天空
-    rgbeLoader();
+    // rgbeLoader();
     animate();
-    // 组件销毁时移除app应用
+    // 组件销毁
     return () => {
-      document.getElementById('container').removeChild(renderer.current.domElement);
+      beforeDestroy();
     };
   }, []);
 
+  // 销毁前
+  const beforeDestroy = () => {
+    gui.destroy();
+    // 组件销毁时移除监听事件
+    window.removeEventListener('resize', resizeUpdate);
+    document.getElementById('container').removeChild(renderer.current.domElement);
+  };
+
+  // 设置灯光
+  const initLight = () => {
+    lighter.ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+    scene.current.add(lighter.ambientLight);
+
+    lighter.directLight = new THREE.DirectionalLight(0xffffff, 2);
+    lighter.directLight.position.set(10, 0, 200);
+    scene.current.add(lighter.directLight);
+  };
+
+  // 设置GUI
+  const initGUI = () => {
+    gui = new GUI();
+    const colorObj = {
+      color: '#ffffff',
+    };
+    const folder_light = gui.addFolder('Lighting');
+    folder_light.open();
+    folder_light
+      .add(lighter.ambientLight, 'intensity', 0, 2)
+      .name('ambientLight')
+      .onChange((value) => {
+        renderer.current.render(scene.current, camera.current); // 当强度更改时重新渲染场景
+      });
+    folder_light
+      .add(lighter.directLight, 'intensity', 0, 4)
+      .name('directLight')
+      .onChange((value) => {
+        renderer.current.render(scene.current, camera.current); // 当强度更改时重新渲染场景
+      });
+
+    folder_light
+      .addColor(colorObj, 'color')
+      .name('ambientColor')
+      .onChange((value) => {
+        lighter.ambientLight.color.set(value);
+      });
+    folder_light
+      .addColor(colorObj, 'color')
+      .name('directColor')
+      .onChange((value) => {
+        lighter.directLight.color.set(value);
+      });
+  };
+
   // 模型加载器
   const gltfLoader = () => {
-    new GLTFLoader().load('../src/resources/models/donuts.glb', (gltf) => {
+    new GLTFLoader().load('./src/resources/models/ImageToStl.com_test.glb', (gltf) => {
       console.log('gltf', gltf);
+      sch = gltf.scene;
 
-      donuts = gltf.scene;
-
-      scene.current!.add(donuts);
-      // donuts.position.set(0, 0, 0);
-      // donuts.rotateX(Math.PI);
-      mixer = new THREE.AnimationMixer(donuts);
-      const clips = gltf.animations;
-      // 播放所有动画
-      clips.forEach((clip) => {
-        const action = mixer.clipAction(clip);
-        action.loop = THREE.LoopOnce;
-        // 停在最后一帧
-        action.clampWhenFinished = true;
-        action.play();
-      });
+      scene.current!.add(sch);
+      sch.position.set(0, 0, 0);
+      sch.rotateX(Math.PI);
     });
   };
 
   // 纹理加载器
   const rgbeLoader = () => {
-    new RGBELoader().load('../src/resources/sky2.hdr', function (texture) {
+    new RGBELoader().load('@/resources/sky2.hdr', function (texture) {
       scene.current!.background = texture;
       texture.mapping = THREE.EquirectangularReflectionMapping;
       scene.current!.environment = texture;
@@ -124,12 +163,6 @@ function App() {
   const animate = () => {
     renderer.current.render(scene.current, camera.current);
     controls.current.update();
-    if (donuts) {
-      donuts.rotation.y += 0.01;
-    }
-    if (mixer) {
-      mixer.update(0.02);
-    }
     requestAnimationFrame(animate);
   };
 
